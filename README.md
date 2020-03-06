f2d3e392d6d64ebb1f680d5717998c2e5c0ac5b1
Chema Muñoz <chemunoz78@gmail.com>
2020-03-06 20:13:23 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# scraping-AS-table
+
+Web Scraping of the classification table of the spanish's football First Division of newspaper AS
+
+# Tools
+
+- Node
+- Puppeteer
---
+++ index.js
@@ -0,0 +1,53 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  try {
+    const navegador = await puppeteer.launch();
+    const pagina = await navegador.newPage();
+    await pagina.goto('https://resultados.as.com/resultados/futbol/primera/clasificacion/');
+
+    let tabla = await pagina.evaluate(() => {
+      const equipos = [...document.querySelectorAll('.nombre-equipo')].map((nodoEquipo) => nodoEquipo.innerText)
+      const puntos = [...document.querySelectorAll("td.destacado")].map((nodoPuntos) => nodoPuntos.innerText)
+      return equipos.map((equipo, index) => ({
+        equipo: equipo,
+        puntos: puntos[index]
+      }))
+    })
+
+    const cmp = (x, y) => {
+      // note when you use the function that the minus before -cmp, for descending order
+      return (x > y) ? 1 : (x < y) ? -1 : 0;
+    };
+
+    let tablas = {
+      total: tabla.slice(0, 20).sort((a, b) => {
+        return cmp(
+          [-cmp(parseInt(a.puntos), parseInt(b.puntos)), -cmp(a.equipo, b.equipo)],
+          [-cmp(parseInt(b.puntos), parseInt(a.puntos)), -cmp(b.equipo, a.equipo)]
+        );
+      }),
+      casa: tabla.slice(20, 40).sort((a, b) => {
+        return cmp(
+          [-cmp(parseInt(a.puntos), parseInt(b.puntos)), -cmp(a.equipo, b.equipo)],
+          [-cmp(parseInt(b.puntos), parseInt(a.puntos)), -cmp(b.equipo, a.equipo)]
+        );
+      }),
+      fuera: tabla.slice(40, 60).sort((a, b) => {
+        return cmp(
+          [-cmp(parseInt(a.puntos), parseInt(b.puntos)), -cmp(a.equipo, b.equipo)],
+          [-cmp(parseInt(b.puntos), parseInt(a.puntos)), -cmp(b.equipo, a.equipo)]
+        );
+      })
+    }
+
+    Object.keys(tablas).forEach((key) => {
+      console.log(`\n${key.toUpperCase()}`)
+      console.table(tablas[key]);
+    });
+
+    await navegador.close();
+  } catch (e) {
+    console.log("ERROR", e)
+  }
+})();
